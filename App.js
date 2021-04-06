42b58ccec3ccae8b15f44e1bdca713363da0f647
Aurelio D'Urso <aurelio@MBPdiAurelio2.homenet.telecomitalia.it>
2021-04-06 15:50:35 +0200

    inizializzazione progetto e primi esperimenti con l'implementazione della fotocamera

--- App.js
+++ App.js
@@ -1,15 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import * as Permissions from 'expo-permissions';
+import { StyleSheet, Text, View ,Image /*ScrollView, TextInput*/, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={() => takePicture()}> 
+          <Image source={require("./images/camera.png")} style={{width: 100, height: 100}} />
+        </TouchableOpacity>
+
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
+
+ /*  return (
+    <View style={styles.container}>
+      <Text>Benvenuto mbare</Text>
+      <StatusBar style="auto" />
+    </View>
+  ); */
+
+
 }
+/* 
+const App = () => {
+  return (
+    <ScrollView>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
+  );
+}
+
+export default App; */
+
+const takePicture = async () => {
+  if (this.camera) {
+      const options = {quality: 1, base64: true};
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data);
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +98,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  /* react camera */
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 });
